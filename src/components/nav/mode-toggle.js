ae6085de39225489572eadf0d392850834295a60
Beatriz Mendes <bea.smendes98@gmail.com>
2021-03-28 02:50:30 +0100

    Add dark mode toggle

--- src/components/nav/mode-toggle.js
+++ src/components/nav/mode-toggle.js
@@ -1,48 +1,48 @@
-import React, { useState } from "react";
-import { createUseStyles, ThemeProvider } from "react-jss";
-import DarkModeToggle from "react-dark-mode-toggle";
+import React from "react";
 import { ThemeToggler } from "gatsby-plugin-dark-mode";
 import { ThemeContext } from "./navbar";
+import "../../css/darkmode-toggle.scss";
 
-const useStyles = createUseStyles({
-    darkToogle: {
-        height: "25px",
-    },
-});
-
-
-export const ModeToggle = ({ big }) => {
-    const classes = useStyles();
-
-    return (
-        <ThemeContext.Consumer>
-            {({ setGlobalTheme }) => (
-                <ThemeToggler>
-                    {({ theme, toggleTheme }) => {
-                        if (theme === null || theme === undefined) {
-                            return null;
-                        } else {
-                            setGlobalTheme(theme);
-                        }
-                        return (
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    onChange={(e) => {
-                                        setGlobalTheme(e.target.checked ? "dark" : "light");
-                                        toggleTheme(e.target.checked ? "dark" : "light");
-                                    }}
-                                    checked={theme === "dark"}
-                                />{" "}
-                                Dark mode
+export const ModeToggle = () => (
+    <ThemeContext.Consumer>
+        {({ setGlobalTheme }) => (
+            <ThemeToggler>
+                {({ theme, toggleTheme }) => {
+                    if (theme === null || theme === undefined) {
+                        return null;
+                    } else {
+                        setGlobalTheme(theme);
+                    }
+                    return (
+                        <div className="toggleWrapper">
+                            <input
+                                checked={theme === "dark"}
+                                type="checkbox" className="dn" id="dn"
+                                onChange={(e) => {
+                                    setGlobalTheme(e.target.checked ? "dark" : "light");
+                                    toggleTheme(e.target.checked ? "dark" : "light");
+                                }}
+                            />
+                            <label htmlFor="dn" className="toggle">
+                                <span className="toggle__handler">
+                                    <span className="crater crater--1" />
+                                    <span className="crater crater--2" />
+                                    <span className="crater crater--3" />
+                                </span>
+                                <span className="star star--1" />
+                                <span className="star star--2" />
+                                <span className="star star--3" />
+                                <span className="star star--4" />
+                                <span className="star star--5" />
+                                <span className="star star--6" />
                             </label>
-                        );
+                        </div>
+                    );
 
-                    }}
-                </ThemeToggler>
-            )}
-        </ThemeContext.Consumer>
-    );
-};
+                }}
+            </ThemeToggler>
+        )}
+    </ThemeContext.Consumer>
+);
 
 export default ModeToggle;
